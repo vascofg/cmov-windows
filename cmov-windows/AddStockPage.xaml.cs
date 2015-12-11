11e4bfe5810b0b814bb83a702dce42146c5cf039
Vasco Gonçalves <vascofg@gmail.com>
2015-12-11 17:02:54 +0000

    Back button on Add Stock page

--- cmov-windows/AddStockPage.xaml.cs
+++ cmov-windows/AddStockPage.xaml.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -28,6 +30,25 @@ namespace MasterDetailApp
             this.InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // Register for hardware and software back request from the system
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested += DetailPage_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested -= DetailPage_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
         private async void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
             if(sender==AppBarAddButton)
@@ -36,6 +57,22 @@ namespace MasterDetailApp
                 Frame rootFrame = Window.Current.Content as Frame;
                 rootFrame.Navigate(typeof(MasterDetailPage));
             }
+        }
+
+        private void OnBackRequested()
+        {
+            // Page above us will be our master view.
+            // Make sure we are using the "drill out" animation in this transition.
+
+            Frame.GoBack(new DrillInNavigationTransitionInfo());
+        }
+
+        private void DetailPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            // Mark event as handled so we don't get bounced out of the app.
+            e.Handled = true;
+
+            OnBackRequested();
         }
     }
 }
