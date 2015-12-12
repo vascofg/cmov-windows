60d0c5be6c84139876521c3677d299e004cbe5a4
wso277 <oliveira.wilson93@gmail.com>
2015-12-12 17:09:09 +0000

    remove cronjob

--- cmov-node/handlers/handlers.js
+++ cmov-node/handlers/handlers.js
@@ -95,7 +95,7 @@ var cronJob = cron.job('*/10 * * * * *', function(){
 
     console.info('cron job completed');
 });
-cronJob.start();
+//cronJob.start();
 
 exports.testHandler = function (request, reply) {
 
